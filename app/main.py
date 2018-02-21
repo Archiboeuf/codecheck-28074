d3123c819ad4c86d9eb8313fc517de700321066c
code-project <dev@code-check.io>
2018-02-21 16:03:17 +0000

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+def main(argv):
+    # このコードは引数と標準出力を用いたサンプルコードです。
+    # このコードは好きなように編集・削除してもらって構いません。
+    # ---
+    # This is a sample code to use arguments and outputs.
+    # Edit and remove this code as you like.
+
+    def hello_world(string):
+        return("Hello {}!".format(string))
+    
+    for i, v in enumerate(argv):
+        print(hello_world(argv[i]))
+        
+#    for i, v in enumerate(argv):
+#        print("argv[{0}]: {1}".format(i, v))
+
+
